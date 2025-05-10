bf02100f24ab431a2359194c1e99abf4a7813cc4
Eric Anderson <eanders@microsoft.com>
2025-05-10 09:12:04 -0700

    initial commit

---
+++ main.ts
@@ -0,0 +1,51 @@
+namespace PlayTogether {
+    let initialized = false;
+    let _isHost = false;
+    let _playerId: string;
+
+    export function isHost() {
+        return _isHost;
+    }
+
+    export function playerId() {
+        return _playerId;
+    }
+
+    export function _init() {
+        if (initialized) return;
+        initialized = true;
+
+        control.simmessages.onReceived(_Protocol.CHANNEL_ID, (buf: Buffer) => {
+            let msg = JSON.parse(buf.toString()) as _Protocol.Message;
+            if (!msg || !msg.type) return;
+            switch (msg.type) {
+                case "host-init": {
+                    const mmsg = msg as _Protocol.HostInitMessage;
+                    _isHost = mmsg.payload.isHost;
+                    _playerId = mmsg.payload.playerId;
+                    break;
+                }
+                case "player-joined": {
+                    const mmsg = msg as _Protocol.PlayerJoinedMessage;
+                    break;
+                }
+                case "player-left": {
+                    const mmsg = msg as _Protocol.PlayerLeftMessage;
+                    break;
+                }
+            }
+        });
+
+        control.simmessages.send(
+            _Protocol.CHANNEL_ID,
+            Buffer.fromUTF8(JSON.stringify(<_Protocol.ClientInitMessage>{
+                type: "client-init",
+                payload: {
+                    version: _Protocol.VERSION,
+                },
+            }))
+        );
+    }
+}
+
+PlayTogether._init();
---
+++ protocol.ts
@@ -0,0 +1,57 @@
+namespace PlayTogether {
+    export namespace _Protocol {
+        export const CHANNEL_ID = "arcade/playtogether";
+        export const VERSION = "1";
+
+        export interface Message {
+            type: string;
+        }
+
+        /**
+         * Client --> Host
+         * Notify host that the client is a PlayTogether client.
+         */
+        export interface ClientInitMessage extends Message {
+            type: "client-init";
+            payload: {
+                version: string;
+            };
+        }
+
+        /**
+         * Host --> Client
+         * Communicate game config and other information to the client.
+         */
+        export interface HostInitMessage extends Message {
+            type: "host-init";
+            payload: {
+                playerId: string; // ID of the local player
+                isHost: boolean; // true if the local player is the session host
+            };
+        }
+
+        /**
+         * Host --> Client
+         * Notify client that a player is joining the game.
+         */
+        export interface PlayerJoinedMessage extends Message {
+            type: "player-joined";
+            payload: {
+                playerId: string;
+                playerName: string;
+                //playerIcon: Buffer;
+            };
+        }
+
+        /**
+         * Host --> Client
+         * Notify client that a player is leaving the game.
+         */
+        export interface PlayerLeftMessage extends Message {
+            type: "player-left";
+            payload: {
+                playerId: string;
+            };
+        }
+    }
+}
